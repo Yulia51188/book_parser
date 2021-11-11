8e821f161a1cca3c3c87ccd81e0e571ac3d9986f
Yulia51188 <sun51188@gmail.com>
2021-11-11 15:11:55 +0300

    Add command line argumets `start_page` and `end_page`

--- parser_by_category.py
+++ parser_by_category.py
@@ -26,9 +26,14 @@ def parse_arguments():
         help='genre (category) page url',
     )
     parser.add_argument(
-        '-p', '--pages_count',
+        '-s', '--start_page',
         type=int,
-        default=4,
+        default=1,
+        help='start page index to parse, default is 1',
+    )
+    parser.add_argument(
+        '-e', '--end_page',
+        type=int,
         help='number of pages to parse',
     )
     return parser.parse_args()
@@ -56,13 +61,28 @@ def parse_book_urls(soup, root_url):
     return book_urls
 
 
-def parse_some_category_pages(category_url, count):
-    if count == 1:
-        page_urls = []
-    else:
-        page_urls = [urljoin(category_url, f'{page_index}')
-                     for page_index in range(2, count + 1)]
-    page_urls.insert(0, category_url)
+def parse_max_page(category_url):
+    soup = fetch_page_soup(category_url)
+
+    max_page_selector = 'p.center a.npage:last-child'
+    max_page = soup.select_one(max_page_selector)
+    return int(max_page.text)
+
+
+def parse_some_category_pages(category_url, start_page, end_page):
+    max_page_index = parse_max_page(category_url)
+
+    if start_page > max_page_index:
+        raise ValueError(f'Only {max_page_index} pages are found. '
+                         'Restart and input correct page indexes')
+
+    if not end_page or end_page > max_page_index:
+        end_page = max_page_index
+        logger.warning('Correct input end page index to maximum available - '
+                       f'{end_page}')
+
+    page_urls = [urljoin(category_url, f'{page_index}')
+                 for page_index in range(start_page, end_page + 1)]    
 
     book_urls = []
     for page_url in page_urls:
@@ -107,7 +127,18 @@ def download_books_by_urls(book_urls):
 def main():
     args = parse_arguments()
 
-    book_urls = parse_some_category_pages(args.category_url, args.pages_count)
+    if args.start_page < 1:
+        args.start_page = 1
+        logger.warning('Start page index corrected to 1')
+    if args.end_page and args.end_page < args.start_page:
+        raise ValueError('Input page indexes range is wrong: '
+                         f'from {args.start_page} to {args.end_page}')
+
+    book_urls = parse_some_category_pages(
+        args.category_url,
+        args.start_page,
+        args.end_page
+    )
     library = download_books_by_urls(book_urls)
 
     with open("library.json", "w") as write_file:
